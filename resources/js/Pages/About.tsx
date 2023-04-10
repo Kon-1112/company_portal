b9c2c0ab3c64794d693fb07afda2befc0e32828c
今野 颯太 <line.kon.1112@gmail.com>
2023-04-10 23:27:53 +0900

    追加：環境構築

---
+++ resources/js/Pages/About.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Link} from "@inertiajs/react";
+
+const About = () => {
+    return (
+        <div>
+        <h1>Abouts</h1>
+            <Link href={"/"}>d</Link>
+            <br/>
+            <Link href={"/about"}>about</Link>
+        </div>
+    );
+}
+
+export default About;
