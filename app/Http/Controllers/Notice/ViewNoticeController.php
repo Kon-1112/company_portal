3084c3352ec76c498501e4e47820876b840c6e8c
今野 颯太 <line.kon.1112@gmail.com>
2023-04-26 00:01:49 +0900

    修正：不要な記述を削除

--- app/Http/Controllers/Notice/ViewNoticeController.php
+++ app/Http/Controllers/Notice/ViewNoticeController.php
@@ -3,43 +3,16 @@
 namespace App\Http\Controllers\Notice;
 
 use App\Http\Controllers\Controller;
-use App\Models\MenuItem;
-use App\Service\NoticeGenreService;
-use App\Service\SideMenu\MenuCategoryService;
-use App\Service\SideMenu\MenuItemService;
-use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class ViewNoticeController extends Controller
 {
 
-    private NoticeGenreService $noticeGenreService;
-
-    private MenuCategoryService $menuCategoryService;
-
-    private MenuItemService $menuItemService;
-
-    /**
-     * コンストラクタ
-     */
-    public function __construct(
-        NoticeGenreService $noticeGenreService,
-        MenuCategoryService $menuCategoryService,
-        MenuItemService $menuItemService
-    )
-    {
-        $this->noticeGenreService = $noticeGenreService;
-        $this->menuCategoryService = $menuCategoryService;
-        $this->menuItemService = $menuItemService;
-    }
-
     // 社内連絡を表示する
     public function internalCommunicationView(): Response
     {
         return Inertia::render('Notice/Home', [
-//            'menuItem'      => $this->menuItemService->getMenuItems(),
-//            'menuCategory'  => $this->menuCategoryService->getMenuCategories(),
             'status'        => session('status'),
         ]);
     }
@@ -48,8 +21,6 @@ class ViewNoticeController extends Controller
     public function importantCommunicationView(): Response
     {
         return Inertia::render('Notice/Home', [
-//            'menuItem'      => $this->menuItemService->getMenuItems(),
-//            'menuCategory'  => $this->menuCategoryService->getMenuCategories(),
             'status' => session('status'),
         ]);
     }
